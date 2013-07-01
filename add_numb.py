10584afd72da88a60da0eda88191d6a0b82d0089
David John Gagne II <djgagne@ou.edu>
2013-07-01 15:36:40 -0500

    Added add_four function

--- add_numb.py
+++ add_numb.py
@@ -1,5 +1,9 @@
 #Authors: David John and John David
 # and Jing is trying to puch back
+
+def add_four(a,b,c,d):
+    return a+b+c+d
+
 from adding import add_three
 def add_two(a,b):
     return a+b
@@ -8,5 +12,6 @@ def main():
     print "Blah"
     print ("2 + 3 = ",add_two(2,3))
     print ("2 + 3 + 4 = ", add_three(2,3,4))
+    print add_four(3,6,2,1)
 if __name__ == "__main__":
     main()
