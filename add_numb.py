f7cc3b577e6e764526e8b3c2e928b93637f9cc7d
David John Gagne II <djgagne@ou.edu>
2013-07-01 14:19:04 -0500

    Added adding.py and modified add_numb to import it

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,10 @@
+from adding import add_three
 def add_two(a,b):
     return a+b
 
 def main():
     print "Blah"
     print ("2 + 3 = ",add_two(2,3))
+    print ("2 + 3 + 4 = ", add_three(2,3,4))
 if __name__ == "__main__":
     main()
---
+++ adding.py
@@ -0,0 +1,3 @@
+def add_three(a,b,c):
+    return a+b+c
+
